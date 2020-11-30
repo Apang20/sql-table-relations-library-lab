cb56dfd6b933516d7bf9ad70a716597e1d96b0f7
github username <github email address>
2020-11-29 18:14:21 -0600

    commit tree 4c8d197382537714b80dcdc31e947ebe1bb97e63

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series JOIN books ON books.series_id = series.id JOIN character_books ON character_books.book_id = books.id JOIN characters 
-  ON character_books.character_id = characters.id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1"
+  ON character_books.character_id = characters.id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
   #"SELECT series.title FROM series JOIN characters ON series.id = characters.series_id WHERE characters.species = 'mermaid' 
   #GROUP BY series.title ORDER BY COUNT(series.title) DESC LIMIT 1;"
 end
